7f4b55455fe1daff52b332e8a9360031fc033741
brunosw1 <bru.pinto@duocuc.cl>
2025-09-10 00:11:20 -0300

    Version Final- 1era entrega
    Se agrego cerrar sesion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,52 @@
-import { Component} from '@angular/core';
-import { IonChip, IonButton, IonIcon, IonCard, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonAvatar, IonLabel, IonSearchbar } from '@ionic/angular/standalone';@Component({
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import {
+  IonChip,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  IonAvatar,
+  IonLabel,
+  IonSearchbar
+} from '@ionic/angular/standalone';
+
+@Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonChip, IonButton, IonIcon, IonCard, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonAvatar, IonLabel, IonSearchbar],
+  imports: [
+    IonChip,
+    IonButton,
+    IonIcon,
+    IonCard,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenu,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonAvatar,
+    IonLabel,
+    IonSearchbar,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private navCtrl: NavController
+  ) {}
+
+  onLogout() {
+    this.authService.logout();
+    this.navCtrl.navigateRoot('/login');
+  }
 }
